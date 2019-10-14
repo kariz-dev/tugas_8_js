730f0ed86951d02d557c247be4b2037552ea0f08
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-15 02:02:57 +0700

    tugas 8 js/ Muhammad Rizwan

---
+++ tugas8.js
@@ -0,0 +1,23 @@
+/* tugas 8 javascript */
+// Muhammad Rizwan
+
+function biodata(){
+    var siswa = {
+        nama : 'Emma Watson',
+        nim : 82456474,
+        fakultas : 'Desain Komunikasi Visual',
+        alamat : 'jl.Bhineka II, Jakarta Timur'
+    }
+
+    // isi semua objek
+    console.log(siswa);
+
+    // sebelum dirubah
+    console.log(siswa.alamat);
+
+    // setelah dirubah
+    siswa.alamat = 'jl.Cendana V, Jakarta Selatan'
+    console.log(siswa);
+}
+
+biodata();
